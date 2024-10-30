706c76c2e5ce02fe1de002618b1f587740ebcdf5
alejandrohdez <alejandrohdezles@gmail.com>
2024-10-30 10:34:42 +0100

    utils modular

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import os
 import torch
+import numpy as np
+import random
 
 def get_device():
     return torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -7,3 +9,12 @@ def get_device():
 def ensure_dir(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
+
+def set_seeds(seed=42):
+    """Set seeds for reproducibility."""
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
